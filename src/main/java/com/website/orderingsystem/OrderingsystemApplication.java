ac0bd1289d840f38806a47203d8930cf6d189838
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-08-05 20:42:44 -0400

    Spring Boot project created

---
+++ src/main/java/com/website/orderingsystem/OrderingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.website.orderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/website/orderingsystem/OrderingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.orderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
